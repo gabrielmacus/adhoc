01cb53586d9e76ebba3019ffba85145907ced2b9
Gabriel <gabrielmacus@gmail.com>
2017-02-16 15:55:58 -0300

    Implementado patron DAO

--- includes/templates/equipos/list.php
+++ includes/templates/equipos/list.php
@@ -15,27 +15,29 @@
             <figure>
                 <img src="<?php echo $bandera["src"]["o"];?>">
             </figure>
-            <h3>Equipacion</h3>
 
-                    <ul>
-                        <?php
-
-                        var_dump($item["jugadores"]);
-                        foreach ($item["jugadores"] as $jugador)
-                        {
-                            ?>
-
-                            <div>
-                                <span><?php echo $jugador["jugador_nombre"];?></span>
-                                <span><?php echo $jugador["jugador_apellido"];?></span>
-                                <span><?php echo $jugador["jugador_peso"];?></span>
-                            </div>
-
-                            <?php
-                        }?>
-                    </ul>
              <?php
                 }?>
+
+            <h3>Equipacion</h3>
+
+            <ul>
+                <?php
+
+                var_dump($item["jugadores"]);
+                foreach ($item["jugadores"] as $jugador)
+                {
+                    ?>
+
+                    <div>
+                        <span><?php echo $jugador["jugador_nombre"];?></span>
+                        <span><?php echo $jugador["jugador_apellido"];?></span>
+                        <span><?php echo $jugador["jugador_peso"];?></span>
+                    </div>
+
+                    <?php
+                }?>
+            </ul>
 
 
 
