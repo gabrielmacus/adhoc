7bd81fbac9a8cb03ef6de7ca9baefe95bb1e4801
Gabriel <gabrielmacus@gmail.com>
2017-03-15 11:33:20 -0300

    archivos multiple

--- includes/framework/helpers/files.php
+++ includes/framework/helpers/files.php
@@ -191,7 +191,8 @@ function uploadFiles($files,$dir,$config)
                     if (ftp_put($conn_id,$completeName, $tmpFile, FTP_BINARY)) {
 
 
-                        $file["sizes"]["o"]["completeUrl"]=$config["dns"].str_replace($config["root_dir"],"",$completeName);
+                        $file["sizes"]["o"]["completeUrl"]=$config["dns"].$dir.$completeName;/*str_replace($config["root_dir"],"",$completeName);/*/
+
 
 
 
