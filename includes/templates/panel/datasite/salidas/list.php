d9850558f9444ba9bc56eca4eb7e62a7b1b7f0cc
Gabriel <gabrielmacus@gmail.com>
2017-03-22 13:59:32 -0300

    salidas

--- includes/templates/panel/datasite/salidas/list.php
+++ includes/templates/panel/datasite/salidas/list.php
@@ -1,4 +1,41 @@
+<div class="row">
+    <?php
 
+    foreach ($dataToSkin as $y =>$m)
+    {
+        ?>
+        <h3 class="teal white-text lighten-1" style="padding: 10px;font-size: 25px">Salidas <?php echo $y?></h3>
+
+
+        <?php
+        foreach ($m as $k=>$v)
+        {
+?>
+
+            <ul class="collection with-header" style="width: 100%">
+                <li class="collection-header"><h4><?PHP  var_dump($k)?></h4></li>
+                <li class="collection-item row">
+                    <div class="col s12 m6">
+                        <strong class="chip">7:30 p.m</strong>
+                    </div>
+                </li>
+                <li class="collection-item">Alvin</li>
+                <li class="collection-item">Alvin</li>
+                <li class="collection-item">Alvin</li>
+            </ul>
+            <?php
+
+            foreach ($v as $item)
+            {
+
+            }
+
+
+        }
+    }
+    ?>
+
+</div>
 
 <div class="row">
     <h3 class="teal white-text lighten-1" style="padding: 10px;font-size: 25px">Salidas 2017</h3>
